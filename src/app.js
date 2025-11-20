cb173333d446c2520ed4072046494775ea49b5fe
pujitha1244 <pujithavankadari1244@gmail.com>
2025-11-20 19:15:19 +0530

    Added Razorpay Integration

--- src/app.js
+++ src/app.js
@@ -3,7 +3,7 @@ const connectDb = require("./config/database");
 const app = express(); //app is instance of express js application, creating new web server
 const cookieParser = require("cookie-parser");
 const cors = require("cors");
-require('dotenv').config()
+require("dotenv").config();
 
 app.use(
   cors({
@@ -18,11 +18,13 @@ const authRouter = require("./routes/auth");
 const profileRouter = require("./routes/profile");
 const requestRouter = require("./routes/requests");
 const userRouter = require("./routes/user");
+const paymentRouter = require("./routes/payment");
 
 app.use("/", authRouter);
 app.use("/", profileRouter);
 app.use("/", requestRouter);
 app.use("/", userRouter);
+app.use("/", paymentRouter);
 
 connectDb()
   .then(() => {
